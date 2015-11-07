ca587281eae03cd72593667ad94c63157b94f815
studyz <evawuhuan@gmail.com>
2015-11-07 19:21:25 +1100

    array method

--- final.pl
+++ final.pl
@@ -1,13 +1,52 @@
 #!/usr/bin/perl -w
-while ($line = <>) { 
-}
+#while ($line = <>) { 
+#}
 #==========================
-while ($ARGV[0]!=0) {
-	print "$ARGV[1]\n";
-	$ARGV[0]--;
-}
-#==========================open file & file to array
-open(F ,"<$f" ) or die "tail.pl: can't open $f \n";
-my @Lines = <F>;
+#while ($ARGV[0]!=0) {
+#	print "$ARGV[1]\n";
+#	$ARGV[0]--;
+#}
+#=====open file & file to array
+#open(F ,"<$f" ) or die "tail.pl: can't open $f \n";
+#my @Lines = <F>;
+
+
+
+#=================
+#===array=========
+#=================
+#@a = (1,3,5);
+#$element=2;
+#push @a, $element;	#add to last
+#pop @a;	#dele last element
+#$last_element = pop @a;
+#@b = reverse(@a);	#reverse array
+#@b = sort(@a);	#sort array
+#shift @a;	#dele left first
+#unshift @a,7;	#add from left first
+
+
+
+
 #===========
-push @files, $element;
+#split array
+#===========
+#$split_string="e q r 2 3 4 r w r";
+#@split_array=split " ",$split_string;
+#foreach $a (@split_array){
+#	print $a."#";
+#}
+#output:e#q#r#2#3#4#r#w#r#
+#===========
+#join array
+#===========
+#$new_split_string=join ",",@split_array;
+#print $new_split_string;
+#output:e,q,r,2,3,4,r,w,r
+
+
+
+
+#==============
+#=======hash===
+#==============
