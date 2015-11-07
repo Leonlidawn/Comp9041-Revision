67cef98115d1e8a35160f33a3649e6ce619e57b6
Leonlidawn <Leonlidawn@gmail.com>
2015-11-07 23:00:03 +1100

    Update final.sh

--- final.sh
+++ final.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
-
+========shell evironment variables ==================
+$0 the name of the command
+$1 the first command-line argument
+$2 the second command-line argument
+$3 the third command-line argument
+$# count of command-line arguments
+$* all of the command-line arguments (together)
+$@ all of the command-line arguments (separately)
+$? exit status of the most recent command
+$$ process ID of this shell
 ==========================grep================== 
 case-insensitive  (-i) 
 
@@ -66,13 +75,41 @@ head -10 | tail -2 copies the 3 to 10 lines
 same as grep -E , egrep allows using of  +, ?, | and ().
 
 grep -F ,Fgrep performs faster with an unique algorithm when only no wildcards used.
-========shell==================
-$0 the name of the command
-$1 the first command-line argument
-$2 the second command-line argument
-$3 the third command-line argument
-$# count of command-line arguments
-$* all of the command-line arguments (together)
-$@ all of the command-line arguments (separately)
-$? exit status of the most recent command
-$$ process ID of this shell
+
+====cut=========
+-fN   n represents the nth column. print the nth columncolumn.
+-cN   n represents the nth positions. print the nth position.
+-dC   use the char c as the field separator.
+eg.
+# print the first column
+cut -f1 data
+# print the first three columns
+cut -f1-3 data
+# print the first and fourth columns
+cut -f1,4 data
+# print all columns after the third
+cut -f4- data
+# print the first three columns, if ’|’-separated
+cut -d’|’ -f1-3 data
+# print the first five chars on each line
+cut -c1-5 data
+=======paste=====
+The paste command displays several text files ”in parallel” on
+output.
+If the inputs are files a, b, c
+the first line of output is composed of the first lines of a, b, c
+the second line of output is composed of the second lines of
+a, b, c
+Lines from each file are separated by a tab character or specified
+delimiter(s).
+-s effect:1	2	3a	b	c
+-d  circularly use listed characters to replace the newline characters instead of the default tab. 
+======sort=======
+-r sort in descending order (reverse sort) eg. e d c b a
+-n sort numerically rather than lexicographically eg. a2 b c d 2 3 4 5 6
+-d dictionary order: ignore non-letters and non-digits 
+-t’c’ use character c to separate columns (default: space)
+-kn’ sort on column n
+Note: the ’ ’ around the separator char are usually not necessary, but
+are useful to prevent the shell from mis-interpreting shell meta-characters
+such as ’|’.
